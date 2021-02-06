b6f17bbf45b28c83c0b2fb764f346acfb0c14704
HaxagonusD <jquezad1@binghamton.edu>
2021-02-06 00:21:30 -0500

    inputs and buttons are there

--- src/App.js
+++ src/App.js
@@ -1,7 +1,43 @@
 import "./App.css";
+import Person from "./services/Person";
+import { useState } from "react";
 
 function App() {
-  return <div className="App"></div>;
+  const [currentName, setCurrentName] = useState("");
+  const [currentEnemies, setCurrentEnemies] = useState("");
+  const [people, setPeople] = useState([]);
+
+  const parseCsv = (csv) => {
+    const csvArray = csv.split(",");
+    return csvArray.map((currentName) => currentName.trim());
+  };
+
+  const addPerson = () => {
+    setPeople([...people, new Person(currentName, currentEnemies)]);
+  };
+
+  return (
+    <div className="App">
+      <input placeholder="Name" />
+      <input
+        type="textarea"
+        placeholder="Enemies"
+        onChange={(event) => setCurrentName(event.target.value)}
+      />
+      <input
+        type="number"
+        placeholder="Number of team members per group"
+        onChange={(event) => setCurrentEnemies(event.target.value)}
+      />
+      <button onClick={addPerson}>Make Teams</button>
+    </div>
+  );
 }
 
 export default App;
+
+//Person
+//has name
+//has list of pople that they don't want to work with
+//
+//Randomly partners people into groups
---
+++ src/services/Person.js
@@ -0,0 +1,7 @@
+export default class Person(){
+  constructor(name, enemies){
+    this.name = name;
+    this.enemies = enemies
+
+  }
+}
