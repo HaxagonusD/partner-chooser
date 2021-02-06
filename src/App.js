6441838b164dcac24810cb195e8ed17862b25d2d
Shafee <shafeelinks@gmail.com>
2021-02-06 01:08:54 -0500

    styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
 import "./App.css";
-import Button from "./components/Button";
+import Person from "./services/Person";
+import { useState } from "react";
 import Header from "./components/Header";
-import InputField from "./components/InputFields";
-import Subtitles from "./components/Subtitles";
-import DisplayPanel from "./panels/DisplayPanel";
-import InputPanel from "./panels/InputPanel";
+import "./css/InputPanel.css";
+import "./css/DisplayPanel.css";
+
+function App() {
+	const [currentName, setCurrentName] = useState("");
+	const [currentEnemies, setCurrentEnemies] = useState("");
+	const [people, setPeople] = useState([]);
+
+	const parseCsv = (csv) => {
+		const csvArray = csv.split(",");
+		return csvArray.map((currentName) => currentName.trim());
+	};
+
+	const addPerson = () => {
+		setPeople([...people, new Person(currentName, currentEnemies)]);
+	};
+
+	return (
+		<div className="App">
+			<div className="input-panel">
+				<Header name={"Team Me"} />
+				<input placeholder="Name" />
+				<input type="textarea" placeholder="No-no's" onChange={(event) => setCurrentName(event.target.value)} />
+				<input type="number" placeholder="Number of team members per group" onChange={(event) => setCurrentEnemies(event.target.value)} />
+				<button onClick={addPerson}>Add person</button>
+			</div>
+			<div className="display-panel">
+				<Header name={"Your generated team:"} />
+			</div>
+		</div>
+	);
+}
+
+export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
 import React from "react";
+import "../css/Header.css";
 
-export default function Header() {
+export default function Header(props) {
 	return (
 		<div>
-			<h1>Team Me</h1>
+			<h1>{props.name}</h1>
 		</div>
 	);
 }
